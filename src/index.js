8b599e3d8b672d7adcb30efd14f904eee96552f3
carlospri <carlospri55@gmail.com>
2025-05-21 15:21:23 -0500

    promesa que usa la funcion .match.random

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-// crear una promesa con error con resolve y rejeact si la variable esta en true esta en error y viceversa
+// crear una promesa que se rechase despues de 3 segundos si un numero aleatorio es <0.5 si no que se resuelva
 
-const promesa1 = new Promise((resolve, reject) => {
-    a=true
-  if (b) {
-    resolve('la promesa tiene un error');
-  } else {
-    reject('se ejecuto con exito');
-  }
-});
+const promesa = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let numeroAleatorio = Math.random()
+        if (numeroAleatorio < 0.5) {
+            resolve(`tuvo exito: el numero Aleatorio es ${numeroAleatorio} y es menor que 0.5`)
+        } else {
+            reject(`tuvo error: el numero Aleatorio es ${numeroAleatorio} y es mayor que 0.5`)
+        }
+    }, 3000)    
+})
+
+promesa().then(resultado => {
+    console.log(resultado)
+}).catch(error => {
+    console.log(error)
+})
